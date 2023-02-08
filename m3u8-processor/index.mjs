96af324f3e8599eab94f463d56f39319d2924d31
sheldar <kewin.polok@gmail.com>
2023-02-08 11:37:34 +0100

    Add multiple resolutions support

--- m3u8-processor/index.mjs
+++ m3u8-processor/index.mjs
@@ -1,26 +1,46 @@
 import ffmpeg from "fluent-ffmpeg";
 import ms from "ms";
+import path from "path";
 
 import { program } from "commander";
 
 const options = program
-  .requiredOption("-i, --input <path>")
-  .requiredOption("-o, --output <path>")
+  .requiredOption("-i, --inputFile <path>", "path to input file")
+  .requiredOption("-o, --outputDirectory <path>", "path to output directory")
+  .option("-r, --resolutions <resolution...>", "output resolutions", [
+    "360",
+    "720",
+  ])
+  .showHelpAfterError()
   .parse()
   .opts();
 
-ffmpeg(input, {
+const inputFile = path.parse(options.inputFile);
+
+const M3U8_EXTENSION = "m3u8";
+
+const command = ffmpeg(options.inputFile, {
   timeout: ms("5m"),
 })
-  .on("progress", function (progress) {
+  .on("start", (commandLine) => {
+    console.log("Spawned ffmpeg with command: " + commandLine);
+  })
+  .on("progress", (progress) => {
     console.log(`Processing: ${progress.percent}% done`);
   })
-  .on("error", function (err) {
-    console.log("Cannot process video: " + err.message);
-    reject();
-  })
   .on("end", () => {
-    console.log(`Finished processing: ${output}`);
-    resolve();
+    console.log(`Finished processing`);
   })
-  .save(output);
+  .on("error", (err) => {
+    console.log("Cannot process video: " + err.message);
+  });
+
+options.resolutions.forEach((resolution) => {
+  command
+    .output(
+      `${options.outputDirectory}/${inputFile.name}_${resolution}p.${M3U8_EXTENSION}`
+    )
+    .size(`?x${resolution}`);
+});
+
+command.run();
