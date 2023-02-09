4d3c9a482037c05d6a70459f07cb76cf6df45be7
sheldar <kewin.polok@gmail.com>
2023-02-09 13:21:05 +0100

    Add react router

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import VideoJs from "../VideoJs";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="info">Check /videojs or /react-player for examples</div>
+    ),
+  },
+  {
+    path: "/videojs",
+    element: <VideoJs />,
+  },
+]);
+
 function App() {
   return (
-    <div className="App">
-      <VideoJs />
+    <div className="app">
+      <RouterProvider router={router} />
     </div>
   );
 }
