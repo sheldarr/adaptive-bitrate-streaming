7d9f6ee156450ab4e497044bae08a87eb704c777
sheldar <kewin.polok@gmail.com>
2023-02-09 13:10:16 +0100

    Move app to components

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-import "./App.css";
-import VideoJs from "./components/VideoJs";
+import VideoJs from "../VideoJs";
+
+import "./index.css";
 
 function App() {
   return (
