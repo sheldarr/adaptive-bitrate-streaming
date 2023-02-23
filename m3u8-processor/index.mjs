67857f99ecf8c8d65b6e2f26b08c47ea48cc2a77
sheldar <kewin.polok@gmail.com>
2023-02-23 18:48:51 +0100

    Prepare and pass all required params to ffmpeg

--- m3u8-processor/index.mjs
+++ m3u8-processor/index.mjs
@@ -1,4 +1,4 @@
-import ffmpeg from "fluent-ffmpeg";
+import { spawn } from "child_process";
 import ms from "ms";
 import path from "path";
 
@@ -7,41 +7,103 @@ import { program } from "commander";
 const options = program
   .requiredOption("-i, --inputFile <path>", "path to input file")
   .requiredOption("-o, --outputDirectory <path>", "path to output directory")
-  .option("-r, --resolutions <resolution...>", "output resolutions", [
-    "360",
-    "720",
-    "1080",
-  ])
   .showHelpAfterError()
   .parse()
   .opts();
+
+const RESOLUTIONS = [
+  {
+    width: "854",
+    height: "480",
+    type: "480p",
+    maxrate: "1500k",
+    bufsize: "2000k",
+  },
+  {
+    width: "1280",
+    height: "720",
+    type: "720p",
+    maxrate: "3000k",
+    bufsize: "4000k",
+  },
+  {
+    width: "1920",
+    height: "1080",
+    type: "1080p",
+    maxrate: "4500k",
+    bufsize: "6000k",
+  },
+  {
+    width: "3840",
+    height: "2160",
+    type: "4k",
+    maxrate: "25500k",
+    bufsize: "34000k",
+  },
+];
 
 const inputFile = path.parse(options.inputFile);
 
 const M3U8_EXTENSION = "m3u8";
 
-const command = ffmpeg(options.inputFile, {
-  timeout: ms("5m"),
-})
-  .on("start", (commandLine) => {
-    console.log("Spawned ffmpeg with command: " + commandLine);
-  })
-  .on("progress", (progress) => {
-    console.log(`Processing: ${progress.percent}% done`);
-  })
-  .on("end", () => {
-    console.log(`Finished processing`);
-  })
-  .on("error", (err) => {
-    console.log("Cannot process video: " + err.message);
-  });
+const inputArgs = ["-hide_banner", "-re", `-i ${options.inputFile}`];
+const mapArgs = RESOLUTIONS.map(() => {
+  return "-map 0:v:0";
+});
+const encodingArgs = [
+  "-c:v h264",
+  "-profile:v main",
+  "-crf 20",
+  "-sc_threshold 0",
+  "-g 48",
+  "-keyint_min 48",
+];
+const filterArgs = RESOLUTIONS.flatMap((resolution, index) => {
+  return [
+    `-filter:v:${index} scale=w=${resolution.width}:h=${resolution.height}`,
+    `-maxrate:v:${index} ${resolution.maxrate}`,
+    `-bufsize:v:${index} ${resolution.bufsize}`,
+  ];
+});
+const varStreamArgs = [
+  `-var_stream_map "${RESOLUTIONS.map((resolution, index) => `v:${index}`).join(
+    " "
+  )}"`,
+  ``,
+];
+const hlsArgs = [
+  "-hls_time 4",
+  "-hls_segment_filename",
+  "output/%v_%03d.ts",
+  "-master_pl_name",
+  "master.m3u8",
+];
+const output = "output/%v.m3u8";
 
-options.resolutions.forEach((resolution) => {
-  command
-    .output(
-      `${options.outputDirectory}/${inputFile.name}_${resolution}p.${M3U8_EXTENSION}`
-    )
-    .size(`?x${resolution}`);
+const ffmpegArgs = [
+  ...inputArgs,
+  ...mapArgs,
+  ...encodingArgs,
+  ...filterArgs,
+  ...varStreamArgs,
+  ...hlsArgs,
+  output,
+];
+
+const ffmpegCommand = `ffmpeg ${ffmpegArgs.join(" ")}`;
+
+console.log(ffmpegCommand);
+
+const ffmpeg = spawn("ffmpeg", ffmpegArgs, { shell: true });
+
+ffmpeg.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
 });
 
-command.run();
+ffmpeg.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ffmpeg.on("close", (code) => {
+  console.log(`ffmpeg process exited with code ${code}`);
+});
