74db3ee251433b84cbb53277b3fed54d43a269cc
sheldar <kewin.polok@gmail.com>
2023-02-23 19:20:40 +0100

    Remove few arcane ffmpeg params

--- m3u8-processor/index.mjs
+++ m3u8-processor/index.mjs
@@ -50,14 +50,7 @@ const inputArgs = ["-hide_banner", "-re", `-i ${options.inputFile}`];
 const mapArgs = RESOLUTIONS.map(() => {
   return "-map 0:v:0";
 });
-const encodingArgs = [
-  "-c:v h264",
-  "-profile:v main",
-  "-crf 20",
-  "-sc_threshold 0",
-  "-g 48",
-  "-keyint_min 48",
-];
+const encodingArgs = ["-c:v h264", "-crf 23"];
 const filterArgs = RESOLUTIONS.flatMap((resolution, index) => {
   return [
     `-filter:v:${index} scale=w=${resolution.width}:h=${resolution.height}`,
@@ -69,14 +62,11 @@ const varStreamArgs = [
   `-var_stream_map "${RESOLUTIONS.map((resolution, index) => `v:${index}`).join(
     " "
   )}"`,
-  ``,
 ];
 const hlsArgs = [
   "-hls_time 4",
-  "-hls_segment_filename",
-  "output/%v_%03d.ts",
-  "-master_pl_name",
-  "master.m3u8",
+  "-hls_segment_filename output/%v_%03d.ts",
+  "-master_pl_name master.m3u8",
 ];
 const output = "output/%v.m3u8";
 
@@ -90,8 +80,7 @@ const ffmpegArgs = [
   output,
 ];
 
-const ffmpegCommand = `ffmpeg ${ffmpegArgs.join(" ")}`;
-
+const ffmpegCommand = `ffmpeg \n\t${ffmpegArgs.join("\n\t")}`;
 console.log(ffmpegCommand);
 
 const ffmpeg = spawn("ffmpeg", ffmpegArgs, { shell: true });
