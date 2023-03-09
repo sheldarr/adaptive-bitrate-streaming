7c7a920b4768aa046720632a567741a54f7fa291
sheldar <kewin.polok@gmail.com>
2023-03-09 12:09:03 +0100

    Move RESOLUTIONS to constants

--- m3u8-processor/lambda/src/constants/index.js
+++ m3u8-processor/lambda/src/constants/index.js
@@ -2,7 +2,39 @@ const TMP_PATH = "/tmp";
 const INPUT_PATH = `${TMP_PATH}/input`;
 const OUTPUT_PATH = `${TMP_PATH}/output`;
 
+const RESOLUTIONS = [
+  {
+    width: "854",
+    height: "480",
+    type: "480p",
+    maxrate: "1500k",
+    bufsize: "2000k",
+  },
+  {
+    width: "1280",
+    height: "720",
+    type: "720p",
+    maxrate: "3000k",
+    bufsize: "4000k",
+  },
+  {
+    width: "1920",
+    height: "1080",
+    type: "1080p",
+    maxrate: "4500k",
+    bufsize: "6000k",
+  },
+  {
+    width: "3840",
+    height: "2160",
+    type: "4k",
+    maxrate: "25500k",
+    bufsize: "34000k",
+  },
+];
+
 module.exports = {
   INPUT_PATH,
   OUTPUT_PATH,
+  RESOLUTIONS,
 };
--- m3u8-processor/lambda/src/ffmpeg/index.js
+++ m3u8-processor/lambda/src/ffmpeg/index.js
@@ -1,37 +1,6 @@
 const { spawn } = require("child_process");
 
-const { INPUT_PATH, OUTPUT_PATH } = require("../constants");
-
-const RESOLUTIONS = [
-  {
-    width: "854",
-    height: "480",
-    type: "480p",
-    maxrate: "1500k",
-    bufsize: "2000k",
-  },
-  {
-    width: "1280",
-    height: "720",
-    type: "720p",
-    maxrate: "3000k",
-    bufsize: "4000k",
-  },
-  {
-    width: "1920",
-    height: "1080",
-    type: "1080p",
-    maxrate: "4500k",
-    bufsize: "6000k",
-  },
-  {
-    width: "3840",
-    height: "2160",
-    type: "4k",
-    maxrate: "25500k",
-    bufsize: "34000k",
-  },
-];
+const { INPUT_PATH, OUTPUT_PATH, RESOLUTIONS } = require("../constants");
 
 const ffmpeg = (path) => {
   const inputArgs = ["-hide_banner", "-re", `-i ${INPUT_PATH}/${path.base}`];
